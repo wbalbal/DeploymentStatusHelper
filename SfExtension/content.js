8774dc6883953fc33c24425fc2addbbd39d67a03
keytuu <etu_elrhaoulaji.kawtar@emsi-edu.ma>
2022-06-16 23:26:26 +0100

    adding css to the created div and poput over the diff button

--- SfExtension/content.js
+++ SfExtension/content.js
@@ -2,7 +2,8 @@ let data = [{
     'status':'SyncKawkaw',
     'path':'path/path/path1',
     'componentName':'[data-component-id]',
-    'lastDateSync':'15/06/22'
+    'lastDateSync':'15/06/22',
+    'diff':'blablablablablab lanlab labla blablabl ablanlablab labla blablab lanlablabl ablabl ablablan lab lablablabl abla blanlablabla blabl ablabl anla'
 }]
 
 window.onload = () => {
@@ -13,24 +14,34 @@ window.onload = () => {
             let componentItem = document.querySelectorAll(componentName);
             for( let componentItemsItem of componentItem ){
                 const InsertedDiv=document.createElement("div");
-                const InsertedButton=document.createElement("button");
+                const InsertedDivContainer=document.createElement("div");
+                const InsertedButton=document.createElement("div");
+                const divPopup=document.createElement("span");
                 InsertedButton.innerText="diff";
                 InsertedButton.name="diff";
                 InsertedButton.setAttribute("cmpName",componentName);
-                InsertedButton:addEventListener("click",getdiff);
-                InsertedDiv.classList.add("deployment_helper");
-                InsertedButton.classList.add("deployment_helper");
+                //InsertedButton:addEventListener("click",getdiff);
+                InsertedButton.classList.add("tooltip");
+                InsertedDivContainer.classList.add("deployment_helper");
+                InsertedDiv.appendChild(InsertedButton);
                 InsertedDiv.innerText=element.status;
-                InsertedDiv.innerText=element.path;
-                InsertedDiv.innerText=element.lastDateSync;
-                componentItemsItem.parentNode.insertBefore(InsertedDiv,componentItemsItem);
+                InsertedDivContainer.appendChild(InsertedDiv);
+                componentItemsItem.appendChild(InsertedDivContainer);
+                componentItemsItem.classList.add("deployment_helper_parent");
                 InsertedDiv.parentNode.insertBefore(InsertedButton,InsertedDiv);
+                divPopup.classList.add("tooltiptext");
+                divPopup.innerText=element.diff;
+                InsertedButton.appendChild(divPopup);
+                //componentItemsItem.parentNode.insertBefore(InsertedDiv,componentItemsItem);
     }}}, "3000")
+
 }
 function getdiff(element){
     let cmpName = element.getAttribute("cmpName");
     console.log(cmpName);
 }
+
+
 
 
 
